4e62680322f7fa8a4c0cc7ef237ce53c521157ea
Artem <artembabichev@gmail.com>
2024-06-30 17:56:44 +0300

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,17 @@ import 'package:dte_app/route/route_provider.dart';
 import 'package:dte_app/screens/dynamic_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
+
 import 'dart:convert';
 import 'env_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  List<RouteModel> routes = await fetchData();
+  List<RouteModel> routes = await RouteProvider.fetchData();
   runApp(MyApp(routes: routes));
 }
 
-Future<List<RouteModel>> fetchData() async {
-  String url = '${EnvConfig.mainApiUrl}api/menu/ru';
-  final response = await http.get(Uri.parse(url));
-  print(response.body);
 
-  if (response.statusCode == 200) {
-    List<dynamic> data = json.decode(response.body);
-    return data.map((json) => RouteModel.fromJson(json)).toList();
-  } else {
-    throw Exception('Failed to load data');
-  }
-}
 
 class MyApp extends StatelessWidget {
   final List<RouteModel> routes;
@@ -44,7 +33,7 @@ class MyApp extends StatelessWidget {
         title: 'Dynamic Routes App',
         initialRoute: '/',
         onGenerateRoute: (settings) {
-          return _generateRoute(settings, routes);
+          return RouteProvider().generateRoute(settings, routes);
         },
         onUnknownRoute: (settings) {
           return MaterialPageRoute(builder: (context) => UnknownPage());
@@ -53,25 +42,7 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Route<dynamic>? _generateRoute(RouteSettings settings, List<RouteModel> routes) {
-    for (var route in routes) {
-      if (settings.name == route.path) {
-        return MaterialPageRoute(
-          builder: (context) => DynamicPage(routeModel: route),
-        );
-      }
-      if (route.subRoutes != null) {
-        for (var subRoute in route.subRoutes!) {
-          if (settings.name == '${route.path}/${subRoute.path}') {
-            return MaterialPageRoute(
-              builder: (context) => DynamicPage(routeModel: subRoute),
-            );
-          }
-        }
-      }
-    }
-    return null;
-  }
+
 }
 
 class UnknownPage extends StatelessWidget {
--- lib/route/route_provider.dart
+++ lib/route/route_provider.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:dte_app/env_config.dart';
+import 'package:dte_app/screens/dynamic_page.dart';
 import 'package:flutter/material.dart';
 import 'route_model.dart';
+import 'package:http/http.dart' as http;
 
 class RouteProvider with ChangeNotifier {
   List<RouteModel> _routes = [];
@@ -9,6 +14,39 @@ class RouteProvider with ChangeNotifier {
   void setRoutes(List<RouteModel> routes) {
     _routes = routes;
     notifyListeners();
+  }
+
+  Route<dynamic>? generateRoute(RouteSettings settings, List<RouteModel> routes) {
+    for (var route in routes) {
+      if (settings.name == route.path) {
+        return MaterialPageRoute(
+          builder: (context) => DynamicPage(routeModel: route),
+        );
+      }
+      if (route.subRoutes != null) {
+        for (var subRoute in route.subRoutes!) {
+          if (settings.name == '${route.path}/${subRoute.path}') {
+            return MaterialPageRoute(
+              builder: (context) => DynamicPage(routeModel: subRoute),
+            );
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  static Future<List<RouteModel>>  fetchData() async {
+    String url = '${EnvConfig.mainApiUrl}api/menu/ru';
+    final response = await http.get(Uri.parse(url));
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((json) => RouteModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load data');
+    }
   }
 
   List<Map<String, dynamic>> getMenuItems() {
